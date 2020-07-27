27f7c6f156c643a30badfdaed7a54a77de81a163
PNOML <vladtimspnoml@gmail.com>
2020-07-27 11:00:51 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,12 @@ export default {
   computed: {
     layout() {
       return (this.$route.meta.layout || "empty") + "-layout";
-    }
+    },
   },
   components: {
     EmptyLayout,
-    MainLayout
-  }
+    MainLayout,
+  },
 };
 </script>
 
