86d37d028a2f0637da90bd71592c08709fd70e44
PNOML <vladtimspnoml@gmail.com>
2020-07-28 12:19:13 +0300

    create new User and catch Err

--- src/store/auth.js
+++ src/store/auth.js
@@ -2,15 +2,33 @@ import firebase from 'firebase/app'
 
 export default {
   actions: {
+    async register({ commit, dispatch }, { email, password, name }) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        const uid = await dispatch('getUid')
+        await firebase.database().ref(`/users/${uid}/info`).set({
+          budget: 10000,
+          name
+        })
+      } catch (e) {
+        commit('setError', e)
+        throw e
+      }
+    },
     async login({ dispatch, commit }, { email, password }) {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password)
       } catch (e) {
+        commit('setError', e)
         throw e
       }
     },
     async logout() {
       await firebase.auth().signOut()
+    },
+    getUid() {
+      const user = firebase.auth().currentUser
+      return user ? user.uid : null
     }
   }
 }
