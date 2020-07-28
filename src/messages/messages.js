f488b09cbd71fe1ffad796c4a17993e3f4ceae2e
PNOML <vladtimspnoml@gmail.com>
2020-07-28 12:20:05 +0300

    add new Err msg

--- src/messages/messages.js
+++ src/messages/messages.js
@@ -1,3 +1,6 @@
 export default {
-  'logout': 'Вы вышли из системы'
+  "logout": 'Вы вышли из системы',
+  "auth/user-not-found": 'Пользовтеля с таким Email не существует',
+  "auth/wrong-password": 'Неверный пароль',
+  "auth/email-already-in-use": 'Пользователь с таким Email уже существует!'
 }
