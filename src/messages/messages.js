0efe5a131322f0c262a270bc3c4b63836b9368e0
PNOML <vladtimspnoml@gmail.com>
2020-07-31 15:01:14 +0300

    add login msg

--- src/messages/messages.js
+++ src/messages/messages.js
@@ -1,5 +1,6 @@
 export default {
   "logout": 'Вы вышли из системы',
+  "login": 'Вам нужно авторизоваться в системе',
   "auth/user-not-found": 'Пользовтеля с таким Email не существует',
   "auth/wrong-password": 'Неверный пароль',
   "auth/email-already-in-use": 'Пользователь с таким Email уже существует!'
