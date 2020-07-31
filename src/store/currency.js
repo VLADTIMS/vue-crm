cb832b723cd110fc7fcc9d9fc7814f8dd3b66c72
PNOML <vladtimspnoml@gmail.com>
2020-07-31 11:17:57 +0300

    remove logging

--- src/store/currency.js
+++ src/store/currency.js
@@ -8,7 +8,6 @@ export default {
   actions: {
     async fetchCurrency() {
       const key = process.env.VUE_APP_FIXER
-      console.log(key)
       const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
       return await res.json()
     }
