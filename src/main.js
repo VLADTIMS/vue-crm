471a4f46849ea96ee2d328d75681aa5d5a85e852
PNOML <vladtimspnoml@gmail.com>
2020-07-31 13:14:20 +0300

    add tooltip directive

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import dateFilter from './filters/date.filter'
 import currencyFilter from './filters/currency.filter'
 import messagePlugin from './plugins/message.plugin'
+import tooltipDirective from './directives/tooltip.directive'
 import Loader from './components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
@@ -21,6 +22,7 @@ Vue.use(Vuelidate)
 Vue.use(messagePlugin)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 
 const firebaseConfig = {
