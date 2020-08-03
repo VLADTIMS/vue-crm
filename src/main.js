3e19ffafa3f88666384e81c9312764ae0075f561
PNOML <vladtimspnoml@gmail.com>
2020-08-03 14:18:56 +0300

    add locale filter

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import dateFilter from './filters/date.filter'
 import currencyFilter from './filters/currency.filter'
+import locolizeFilter from './filters/locolize.filter'
 import messagePlugin from './plugins/message.plugin'
 import tooltipDirective from './directives/tooltip.directive'
 import Loader from './components/app/Loader'
@@ -23,6 +24,7 @@ Vue.use(Vuelidate)
 Vue.use(messagePlugin)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.filter('locolize', locolizeFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
