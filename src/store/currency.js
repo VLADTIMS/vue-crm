413c032f187b707183f299b33542946c3dda125c
PNOML <vladtimspnoml@gmail.com>
2020-08-03 15:33:10 +0300

    fix http protection on fixer: change url

--- src/store/currency.js
+++ src/store/currency.js
@@ -8,7 +8,8 @@ export default {
   actions: {
     async fetchCurrency() {
       const key = process.env.VUE_APP_FIXER
-      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
+      // const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,RUB`)
+      const res = await fetch(`https://api.exchangerate-api.com/v4/latest/EUR`)
       return await res.json()
     }
   },
